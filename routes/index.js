fc40f55b2471b139fdba1277586e7e4a3beba320
Marco Muller <marco.muller.mm@gmail.com>
2022-03-21 15:53:48 +0100

    lab finished

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const Movie = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
-module.exports = router;
+/*GET movies pages*/
+
+router.get('/movies', async (req, res, next) => {
+    const movies = await Movie.find()
+  
+    res.render('movies', { movies })
+  });
+
+/*GET each movie info page*/
+
+router.get('/movie/:id', async (req, res, next) => {
+    const movie = await Movie.findById(req.params.id)
+  
+    res.render('movie', { movie })
+  })
+
+  module.exports = router;
